73c4a535e6fe044aad43bc70b7ac5c6160bbbde5
diegojrodriguess <diego.rodrigues@ges.inatel.br>
2023-08-20 22:20:19 -0300

    Coin Converter - V1

---
+++ script.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form');
+form.addEventListener('submit',handleSubmit);
+
+const inputValue = document.getElementById('value-real');
+const selectedCurrency = document.getElementById('currency');
+const result = document.getElementById('result');
+
+function handleSubmit(e){
+  e.preventDefault();
+  if(!inputValue.value || inputValue.value < 0){
+    alert('Informe em um valor correto!')
+    return;
+  } else if (!selectedCurrency.value){
+    alert('Escolha uma moeda!')
+    return;
+  }
+  converter();
+};
+
+function converter(){
+    if (selectedCurrency.value === 'eur'){
+        valueConverted = inputValue.value / 5.41;
+        result.innerHTML = valueFormatter('pt-BR','EUR');
+    }   else if (selectedCurrency.value === 'dol'){
+        valueConverted = inputValue.value / 4.97;
+        result.innerHTML = valueFormatter('en-US','USD');
+    }
+
+    inputValue.value = '';
+    selectedCurrency.value = '';
+};
+
+function valueFormatter(Locale,currency){
+    const value = valueConverted.toLocaleString(`${Locale}`, {style: 'currency', currency: `${currency}`});
+    return `${value}`;
+}
